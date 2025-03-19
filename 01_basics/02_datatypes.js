be099d706d3fdd46a51fe392fd80140bb14f90dd
Dynamis-U <saujjwal70@gmail.com>
2025-03-19 12:32:37 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; /* Treat all JS code in newer version. Although it automatically runs everything in strict mode*/
+
+//alert(3 + 3) we are using node.js not browser
+
+console.log(3 
+    + 
+    3); //code readablity should be high
+
+console.log("Ujjwal Sahu")
+
+let name = "Ujjwal"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2^53
+// bigint
+// string =>""
+// boolean =? true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+
+//object
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
