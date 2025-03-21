ecd8c1bc2f1b9d2c3e2eec0d6255fe8cc788a07a
Dynamis-U <saujjwal70@gmail.com>
2025-03-21 12:03:48 +0000

    Comparison and equality checks

--- 01_basics/04_comparison.js
+++ 01_basics/04_comparison.js
@@ -7,10 +7,20 @@
 // console.log("2" > 1);
 // console.log("02" > 1);
 
-console.log(null > 0);  
-console.log(null == 0);
-console.log(null >= 0);
+// console.log(null > 0);  
+// console.log(null == 0);
+// console.log(null >= 0);
 
 // The reason is the equality (==) and comparison (>,<,>=,<=) operators work differntly
 // Comparisons convert null to a number, treating it as 0. Thats why (3) null >= 0 true and (1)null > 0 false
+
+// console.log(undefined == 0);
+// console.log(undefined < 0);
+// console.log(undefined > 0);
+
+// undefined gives false value with both equality check and comparison operator
+
+// ===
+
+console.log("2" === 2); //== gives true because it doesn't do data type check but === gives false as it check datatype also
 
