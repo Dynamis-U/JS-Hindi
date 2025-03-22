bbfb25d39ef4b135d986d9347e3dbcdd2b765213
Dynamis-U <saujjwal70@gmail.com>
2025-03-22 10:41:40 +0000

    Stack and heap

--- 01_basics/datatype-summary.js
+++ 01_basics/datatype-summary.js
@@ -31,3 +31,27 @@ const myFunction = function(){
 }
 
 console.log(typeof myFunction); //actually its datatype is function object
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Stack(Primitive), Heap(Non-Primitive)
+
+let myInstaName = "aloof"
+
+let anotherName = myInstaName
+anotherName = "rizZ"
+
+console.log(anotherName); //rizZ
+console.log(myInstaName); //aloof because it takes copy of myInstaName in a stack so change copy cant change the original value
+
+let userOne = {
+    email: "user@google.com",
+    upi: "user@ybl"
+}
+
+let userTwo = userOne
+
+userTwo.email = "ujjwal@google.com"
+
+console.log(userOne.email);      //In non Primitives like objects it store values at heap and variable have reference to that value 
+console.log(userTwo.email);      //so changing the referencing value change the actual value
