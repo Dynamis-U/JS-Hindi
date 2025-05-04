a1db3a3b9270aa9c681910f2b860932ad592fce4
Dynamis-U <saujjwal70@gmail.com>
2025-05-04 18:01:46 +0000

    can we change the value of pir

--- 10_classes_and_oops/mathpi.js
+++ 10_classes_and_oops/mathpi.js
@@ -9,8 +9,26 @@ const descripter = Object.getOwnPropertyDescriptor(Math, 'PI');
 const chai = {
     name : 'ginger chai',
     price : 250,
-    isAvailable : true
+    isAvailable : true,
+
+    orderChai : function(){
+        console.log("Chai nhi bani");
+    }
 }
+
+// console.log(Object.getOwnPropertyDescriptor(chai, "name"));
+
+Object.defineProperty(chai, 'name', {
+//     writable : false,
+    enumerable : false        // it will stop  iteration now
+})
 
 console.log(Object.getOwnPropertyDescriptor(chai, "name"));
 
+for (let [key, value] of Object.entries(chai)) {          // chai us object and it is directly not iterable although object iterable or not it depends situation to situation so we iterates it by its entries
+    
+    if(typeof value !== 'function'){
+        console.log(`${key} : ${value}`)
+    }
+    
+}
