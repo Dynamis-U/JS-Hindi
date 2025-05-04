c0f6be6f1c14eca27876376bd4e6c5122a5267d9
Dynamis-U <saujjwal70@gmail.com>
2025-05-04 09:45:02 +0000

    object complete

---
+++ 10_classes_and_oops/mathpi.js
@@ -0,0 +1,16 @@
+const descripter = Object.getOwnPropertyDescriptor(Math, 'PI');
+
+// console.log(Math.PI);
+// Math.PI = 5;
+// console.log(Math.PI);
+
+// console.log(descripter);
+
+const chai = {
+    name : "ginger chai",
+    price : 250,
+    isAvailable : true
+}
+
+console.log(object)
+
